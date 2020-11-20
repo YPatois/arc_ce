8247aab13ebcf96b1e79b7f0e0f25c34aad4cf24
Yannick Patois <yannick.patois@iphc.cnrs.fr>
2020-11-20 09:15:45 +0100

    [arex:ws] block added

---
+++ manifests/arex/ws.pp
@@ -0,0 +1,15 @@
+# Class arc_ce::arex::ws
+# Configures the ws blocks in arc.conf
+class arc_ce::arex::ws(
+  Boolean $enable = false,
+) {
+
+  if $enable {
+    concat::fragment { 'arc.conf_arex_ws':
+      target  => '/etc/arc.conf',
+      content => template("${module_name}/arex/ws.erb"),
+      order   => 20,
+    }
+  }
+
+}
